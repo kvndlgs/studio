933f85432c3b42b56a063b41aae6a376863d642f
letsget thisbredd <poissedonn@gmail.com>
2025-08-02 00:57:59 +0000

    the authentication doest work anymore. i get auth/network-request-failed

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,7 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
-import { getFunctions, connectFunctionsEmulator } from 'firebase/functions';
-import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { getFirestore } from 'firebase/firestore';
+import { getFunctions } from 'firebase/functions';
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   "projectId": "suckerpunch",
@@ -17,16 +17,5 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
 const functions = getFunctions(app, 'us-central1');
 const auth = getAuth(app);
-
-// Connect to emulators in development
-if (process.env.NODE_ENV === 'development') {
-    try {
-        connectAuthEmulator(auth, 'http://127.0.0.1:9099', { disableWarnings: true });
-        connectFirestoreEmulator(db, '127.0.0.1', 8080);
-        connectFunctionsEmulator(functions, '127.0.0.1', 5001);
-    } catch (error) {
-        console.error("Error connecting to Firebase emulators:", error);
-    }
-}
 
 export { app, auth, db, functions };
